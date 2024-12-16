9d17d80093b3b8c17546ea296b45d15b4ceabc8d
Prashast0010 <98142590+Prashast0010@users.noreply.github.com>
2024-12-16 20:00:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+let arr[];
+
+
+
+let strt=false;
+
+let algo=document.querySelector(".cntrl");
+addEventListener("click",function()
+{if(strt==false){
+
+   algo.innerHTML="<option>quick</option>"; 
+//    algo.innerHTML="<option>merge</option>"; 
+
+    console.log("clicked");
+
+    strt=true;
+}
+    
+    
+});
+
